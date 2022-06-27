17e0901ae1d0bca295882b89cab56e1071378837
jhonAp25 <apazalaricojhon25@gmail.com>
2022-06-26 20:22:04 -0500

    import esudiante II

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,8 +5,8 @@ insert into Carrera (descripcion) values ('Administracion de Empresas');
 insert into Carrera (descripcion) values ('Ing. Industrial');
 
 
-insert into estudiante (apellido, dni, fecnac, nombre,foto, carrera_id) values ('apaza', '75680099', '2000-05-12','joihn','https://i.imgur.com/3mhhSzi.jpg' , 1);
-insert into estudiante (apellido, dni, fecnac, nombre,foto, carrera_id) values ('suarez', '75680099', '2000-05-12','jose','https://i.imgur.com/IKNorjn.jpg',  1);
-insert into estudiante (apellido, dni, fecnac, nombre,foto, carrera_id) values ('caceres', '75680099', '2000-05-12','Maria','https://i.imgur.com/IKNorjn.jpg',  2);
-insert into estudiante (apellido, dni, fecnac, nombre,foto, carrera_id) values ('Vazques Huaman', '75680099', '2000-05-12','maria', 'https://i.imgur.com/IKNorjn.jpg', 5);
-insert into estudiante (apellido, dni, fecnac, nombre,foto, carrera_id) values ('Larico Delgado', '75680099', '2000-05-12','Daniel Carlos','https://i.imgur.com/pMTKt20.jpg',  4);
+insert into estudiante (apellido, dni, fecnac, nombre,foto,correo, carrera_id) values ('apaza', '75680099', '2000-05-12','joihn','https://i.imgur.com/3mhhSzi.jpg','correo@gmail.com', 1);
+insert into estudiante (apellido, dni, fecnac, nombre,foto,correo, carrera_id) values ('suarez', '75680099', '2000-05-12','jose','https://i.imgur.com/IKNorjn.jpg','correo@outlook.com',  1);
+insert into estudiante (apellido, dni, fecnac, nombre,foto,correo, carrera_id) values ('caceres', '75680099', '2000-05-12','Maria','https://i.imgur.com/IKNorjn.jpg','correo@gmail.com',  2);
+insert into estudiante (apellido, dni, fecnac, nombre,foto,correo, carrera_id) values ('Vazques Huaman', '75680099', '2000-05-12','maria', 'https://i.imgur.com/IKNorjn.jpg','correo@gmail.com', 5);
+insert into estudiante (apellido, dni, fecnac, nombre,foto,correo, carrera_id) values ('Larico Delgado', '75680099', '2000-05-12','Daniel Carlos','https://i.imgur.com/pMTKt20.jpg','correo@gmail.com',  4);
